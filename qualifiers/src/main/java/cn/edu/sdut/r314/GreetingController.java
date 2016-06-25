0bb911bcbaf81894fe13c1f8f359c5af20c645c4
subaochen <subaochen@126.com>
2016-06-25 17:21:39 +0800

    增加遍历组件说明@Any用法的代码

--- qualifiers/src/main/java/cn/edu/sdut/r314/GreetingController.java
+++ qualifiers/src/main/java/cn/edu/sdut/r314/GreetingController.java
@@ -1,15 +1,27 @@
 
 package cn.edu.sdut.r314;
 
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 public class GreetingController {
-    @Inject @Fancy Greeting greeting;
+    @Inject  Greeting greeting;
+    @Inject @Any Instance<Greeting> greetings;
     
     public String sayHello(String name){
         return greeting.greet(name);
+    }
+    
+    public String hello(String name) {
+        String helloString = "";
+        for(Greeting greeting:greetings) {
+            helloString += greeting.greet(name);
+        }
+        
+        return helloString;
     }
     
 }
